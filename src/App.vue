916e6d59a47dcf71b9b270e334fbdf2e7da24cf5
anguyen2526 <anguyen94@unomaha.edu>
2026-02-05 02:00:07 -0600

    push to deploy

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<!--<router-view/> required to route to views - add to each template unless included here-->
+<router-view/>
+</template>
+
+<script>
+import router from "./router";
+export default {
+ name: 'app',
+ props: {
+          msg: String
+      },
+  mounted(){
+    router.push("/skill-list");
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
